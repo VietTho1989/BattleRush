f946dc1b6f5c37fce80c96b1848727924c09a29c
VietTho1989 <doanviettho@gmail.com>
2023-01-13 11:51:55 +0700

    save map editor

--- Assets/BattleRush/Editor/MyMapEditor.cs
+++ Assets/BattleRush/Editor/MyMapEditor.cs
@@ -130,29 +130,7 @@ namespace BattleRushS
             SceneView.duringSceneGui -= this.OnSceneGUI;
         }
 
-        private Vector2 cellSize = new Vector2(2f, 2f);
-
-        private void DisplayVisualHelp(Vector2 cellCenter)
-        {
-            // Get the mouse position in world space such as z = 0
-            //Ray guiRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
-            //Vector3 mousePosition = guiRay.origin - guiRay.direction * (guiRay.origin.z / guiRay.direction.z);
-
-            // Get the corresponding cell on our virtual grid
-            //Vector2Int cell = new Vector2Int(Mathf.RoundToInt(mousePosition.x / cellSize.x), Mathf.RoundToInt(mousePosition.y / cellSize.y));
-            //Vector2 cellCenter = cell * cellSize;
-
-            // Vertices of our square
-            Vector3 topLeft = cellCenter + Vector2.left * cellSize * 0.5f + Vector2.up * cellSize * 0.5f;
-            Vector3 topRight = cellCenter - Vector2.left * cellSize * 0.5f + Vector2.up * cellSize * 0.5f;
-            Vector3 bottomLeft = cellCenter + Vector2.left * cellSize * 0.5f - Vector2.up * cellSize * 0.5f;
-            Vector3 bottomRight = cellCenter - Vector2.left * cellSize * 0.5f - Vector2.up * cellSize * 0.5f;
-
-            // Rendering
-            Handles.color = Color.green;
-            Vector3[] lines = { topLeft, topRight, topRight, bottomRight, bottomRight, bottomLeft, bottomLeft, topLeft };
-            Handles.DrawLines(lines);
-        }
+        private Vector3 cellSize = new Vector3(2f, 0, 2f);
 
         void OnFocus()
         {
@@ -258,15 +236,15 @@ namespace BattleRushS
             GUILayout.EndScrollView();
         }
 
-        private Vector2 GetSelectedCell()
+        private Vector3 GetSelectedCell()
         {
-            // Get the mouse position in world space such as z = 0
+            // Get the mouse position in world space such as y = 0
             Ray guiRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
-            Vector3 mousePosition = guiRay.origin - guiRay.direction * (guiRay.origin.z / guiRay.direction.z);
+            Vector3 mousePosition = guiRay.origin - guiRay.direction * (guiRay.origin.y / guiRay.direction.y);
 
             // Get the corresponding cell on our virtual grid
-            Vector2Int cell = new Vector2Int(Mathf.RoundToInt(mousePosition.x / cellSize.x), Mathf.RoundToInt(mousePosition.y / cellSize.y));
-            return cell * cellSize;
+            Vector3Int cell = new Vector3Int(Mathf.RoundToInt(mousePosition.x / cellSize.x), 1, Mathf.RoundToInt(mousePosition.z / cellSize.z));
+            return Vector3.Scale(cell, cellSize);
         }
 
         // Does the rendering of the map editor in the scene view.
@@ -278,10 +256,46 @@ namespace BattleRushS
                     break;
                 case Controller.State.Type.Paint:
                     {
-                        Vector2 cellCenter = GetSelectedCell(); // Refactoring, I moved some code in this function
+                        Vector3 cellCenter = GetSelectedCell(); // Refactoring, I moved some code in this function
 
-                        DisplayVisualHelp(cellCenter);
-                        HandleSceneViewInputs(cellCenter);
+                        // DisplayVisualHelp(cellCenter);
+                        {
+                            // Vertices of our square
+                            Vector3 topLeft = cellCenter + Vector3.Scale(Vector3.left, cellSize) * 0.5f + Vector3.Scale(Vector3.forward, cellSize) * 0.5f;
+                            Vector3 topRight = cellCenter - Vector3.Scale(Vector3.left, cellSize) * 0.5f + Vector3.Scale(Vector3.forward, cellSize) * 0.5f;
+                            Vector3 bottomLeft = cellCenter + Vector3.Scale(Vector3.left, cellSize) * 0.5f - Vector3.Scale(Vector3.forward, cellSize) * 0.5f;
+                            Vector3 bottomRight = cellCenter - Vector3.Scale(Vector3.left, cellSize) * 0.5f - Vector3.Scale(Vector3.forward, cellSize) * 0.5f;
+
+                            // Rendering
+                            Handles.color = Color.green;
+                            Vector3[] lines = { topLeft, topRight, topRight, bottomRight, bottomRight, bottomLeft, bottomLeft, topLeft };
+                            Handles.DrawLines(lines);
+                        }
+
+                        // HandleSceneViewInputs(cellCenter);
+                        {
+                            // Filter the left click so that we can't select objects in the scene
+                            if (Event.current.type == EventType.Layout)
+                            {
+                                HandleUtility.AddDefaultControl(0); // Consume the event
+                            }
+
+                            // We have a prefab selected and we are clicking in the scene view with the left button
+                            if (controller.paletteIndex.v >= 0 && controller.paletteIndex.v < controller.palette.vs.Count)
+                            {
+                                if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
+                                {
+                                    Logger.Log("MyMapEditor: cell center position: " + cellCenter);
+                                    // Create the prefab instance while keeping the prefab link
+                                    GameObject prefab = controller.palette.vs[controller.paletteIndex.v].getMyGameObject();
+                                    GameObject gameObject = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+                                    gameObject.transform.position = cellCenter;
+
+                                    // Allow the use of Undo (Ctrl+Z, Ctrl+Y).
+                                    Undo.RegisterCreatedObjectUndo(gameObject, "");
+                                }
+                            }
+                        }
 
                         // Refresh the view
                         sceneView.Repaint();
@@ -290,15 +304,6 @@ namespace BattleRushS
                 default:
                     Logger.LogError("unknown type: " + controller.state.v.getType());
                     break;
-            }
-        }
-
-        private void HandleSceneViewInputs(Vector2 cellCenter)
-        {
-            // Filter the left click so that we can't select objects in the scene
-            if (Event.current.type == EventType.Layout)
-            {
-                HandleUtility.AddDefaultControl(0); // Consume the event
             }
         }
 
